a1de2b76fca2de5f9de8be4925db22cfa1255071
gian <michaelgiantiqui3@gmail.com>
2023-12-14 10:40:34 +0800

    m

---
+++ TheBooks/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TheBooks.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
